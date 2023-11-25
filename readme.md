da9117e8786b0d17038c585ab7927d49fcebbf68
锐源 <1370164516@qq.com>
2023-11-25 20:03:45 +0800

    redis

--- readme.md
+++ readme.md
@@ -5,3 +5,6 @@
 # 2、监听用户监听
 - index.js是服务端
 - index.html是客户端
+
+# 3、laravel10使用redis 注意事项
+- .env记得添加：REDIS_CLIENT=predis
--- redis-lesson/routes/web.php
+++ redis-lesson/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redis;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+//    return view('welcome');
+
+    Redis::set('name', 'Lzc');
+
+    return Redis::get('name');
 });
