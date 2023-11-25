3e59477d2edca966172c1faacf4a5a251e800a4d
锐源 <1370164516@qq.com>
2023-11-25 21:27:31 +0800

    连接远端redis

--- redis-lesson/socket.js
+++ redis-lesson/socket.js
@@ -5,7 +5,12 @@ var http = require('http').Server();
 var io = require('socket.io')(http);//监听
 //引入redis,返回的是个对象，所以需要再放到一个对象里面
 var Redis = require('ioredis');
-var redis = new Redis();
+var redis = new Redis({
+    host: '192.168.56.12', // Redis 服务器的 IP 地址或域名
+    port: 6379,              // Redis 服务器的端口号，如果是默认端口可以省略
+    password: '123456',    // 如果设置了密码的话
+    db: 0                    // 如果您想连接到非默认数据库的话
+});
 
 redis.subscribe('test-channel');
 
@@ -13,4 +18,6 @@ redis.on('message', function (channel, msg) {
     console.log(msg);
 });
 
-http.listen(3000);
+http.listen(3000, function () {
+    console.log('Server Start')
+});
